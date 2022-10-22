a93b8bf4ede1aaa708c5ada0995ee6a5e8927996
Febrianto <febrianto.bekasi@gmail.com>
2022-10-22 21:09:53 +0700

    use mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,21 +20,17 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        test: /\.(sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
     }),
   ],
 };
